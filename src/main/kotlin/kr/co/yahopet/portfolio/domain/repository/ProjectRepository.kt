2bf6e02bf0d5558d112ea0e176820c1926a5066d
ldh52 <yahopet79@gmail.com>
2024-11-20 14:38:50 +0900

    리포지토리 성능 개선

--- src/main/kotlin/kr/co/yahopet/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/kr/co/yahopet/portfolio/domain/repository/ProjectRepository.kt
@@ -2,9 +2,16 @@ package kr.co.yahopet.portfolio.domain.repository
 
 import kr.co.yahopet.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
+    @Query(
+        "select p from Project p left join fetch p.skills s join fetch s.skill where " +
+                "p . isActive = :isActive"
+    )
     fun findAllByIsActive(isActive: Boolean): List<Project>
+
+    @Query("select p from Project p join fetch p.details where p.id = :id")
     override fun findById(id: Long): Optional<Project>
 }
