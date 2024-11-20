8853ca5017b63912358fe81765fce7370fc64715
ldh52 <yahopet79@gmail.com>
2024-11-20 12:14:06 +0900

    entity 개발

--- src/main/kotlin/kr/co/yahopet/portfolio/domain/entity/Skill.kt
+++ src/main/kotlin/kr/co/yahopet/portfolio/domain/entity/Skill.kt
@@ -1,13 +1,24 @@
 package kr.co.yahopet.portfolio.domain.entity
 
 import jakarta.persistence.*
+import kr.co.yahopet.portfolio.domain.constant.SkillType
 
 @Entity
-class Skill : BaseEntity() {
+class Skill(
+    name: String,
+    type: String,
+    isActive: Boolean
+) : BaseEntity() {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "skill_id")
     var id: Long? = null
+    var name: String = name
+
+    @Column(name = "skill_type")
+    @Enumerated(value = EnumType.STRING)
+    var type: SkillType = SkillType.valueOf(type)
+    var isActive: Boolean = isActive
 
 }
