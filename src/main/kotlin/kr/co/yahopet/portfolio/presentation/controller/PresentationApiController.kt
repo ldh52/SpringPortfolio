d83c67218fd9dafd383ae8e761fef2e1174bd2dd
ldh52 <yahopet79@gmail.com>
2024-11-20 16:32:24 +0900

    컨트롤러 개발

--- src/main/kotlin/kr/co/yahopet/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/kr/co/yahopet/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,5 +1,10 @@
 package kr.co.yahopet.portfolio.presentation.controller
 
+import kr.co.yahopet.portfolio.presentation.dto.IntroductionDTO
+import kr.co.yahopet.portfolio.presentation.dto.LinkDTO
+import kr.co.yahopet.portfolio.presentation.dto.ProjectDTO
+import kr.co.yahopet.portfolio.presentation.dto.ResumeDTO
+import kr.co.yahopet.portfolio.presentation.service.PresentationService
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,10 +12,33 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+    private val presentationService: PresentationService
+) {
 
     @GetMapping("/test")
     fun test(): String {
         return "OK"
     }
+
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
+
 }
